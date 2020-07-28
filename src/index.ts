68cddef7fdc3f83fb8167442627f86e6b681d8ac
Gideão Silva <gideaoms@gmail.com>
2020-07-28 15:30:22 -0300

    fix params v2

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,24 @@
 import { Context, Next } from 'koa'
 
-const catcher = ({ debug = false, message = '' } = {}) => async (ctx: Context, next: Next) => {
-  try {
-    await next()
-  } catch (error) {
-    ctx.app.emit('error', error, ctx)
-
-    if (debug) {
-      console.info(error)
-    }
-
-    ctx.response.status = error.status ?? error.statusCode ?? 500
-    ctx.response.body = { message: message ?? 'Internal Server Error' }
-  }
+type CatcherOpts = {
+  debug?: boolean
+  message?: string
 }
+
+const catcher = (opts?: CatcherOpts) =>
+  async (ctx: Context, next: Next) => {
+    try {
+      await next()
+    } catch (error) {
+      ctx.app.emit('error', error, ctx)
+
+      if (opts?.debug) {
+        console.info(error)
+      }
+
+      ctx.response.status = error.status ?? error.statusCode ?? 500
+      ctx.response.body = { message: opts?.message ?? 'Internal Server Error' }
+    }
+  }
 
 export default catcher
